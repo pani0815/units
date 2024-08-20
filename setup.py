9e01f71b06c0aa50e1f1e0602953b5d158e90c50
Dominik Panstingl <dominik.panstingl.2003@gmail.com>
2024-08-20 10:36:32 +0200

    changed license

--- setup.py
+++ setup.py
@@ -19,11 +19,8 @@ setup(
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
 
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'License :: OSI Approved :: GNU GENERAL PUBLIC LICENSE',   # Again, pick a license
 
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.11',
   ],
 )
