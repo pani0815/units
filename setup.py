88af5359b15d33856ab91c3017b94846dbc1db01
Dominik Panstingl <dominik.panstingl.2003@gmail.com>
2024-08-20 10:39:31 +0200

    changed download link

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Dominik Panstingl',                   # Type in your name
   author_email = 'dominik.panstingl@gmx.at',      # Type in your E-Mail
   url = 'https://github.com/pani0815/units',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/pani0815/units/archive/refs/heads/main.zip',    # I explain this later on
+  download_url = 'https://github.com/pani0815/units/archive/refs/tags/0.0.tar.gz',    # I explain this later on
   keywords = ['units', 'physics'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy'
